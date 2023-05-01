272614a6e2157df4b2f81899a1fe77d129e00b47
Tyler Swystun <86743463+Awesome012@users.noreply.github.com>
2023-05-01 11:08:42 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -4,7 +4,19 @@ import time
 
 inp = input("press enter to start")
 pyautogui.moveTo(510,882,1)
+pyautogui.click()
 awesome = True
-while awesome:
-    pyautogui.moveTo(1000,484)
-    pyautogui.click()
+def autoclick():
+    while awesome:
+        pyautogui.moveTo(1000,484)
+        pyautogui.click()
+def levelup():
+    level = pyautogui.locateOnScreen('assets/Level Up.png',confidence=0.7)
+    if level == None:
+        autoclick()
+    else:
+        pyautogui.moveTo(level)
+        pyautogui.click()
+        levelup()
+
+autoclick()
