25fc1e1240470b49fe1edcc20a0ab03b3a03ceb9
Tyler Swystun <86743463+Awesome012@users.noreply.github.com>
2023-05-01 11:34:41 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -14,7 +14,7 @@ def autoclick():
         tally = tally + 1
         levelup()
 def levelup():
-    level = pyautogui.locateOnScreen('assets/Level Up.png',confidence=0.8)
+    level = pyautogui.locateOnScreen('assets/Level Up.png',confidence=0.85)
     if level == None:
         autoclick()
     else:
